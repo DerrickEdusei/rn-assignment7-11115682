ec1cdfbbfa4792dadf5d0146ba1d95ee92197a60
Bouffdaddy1z <derrickedus66@gmail.com>
2024-07-11 16:05:52 +0000

    removed and reinstalled expo app due to errors in installing expo framework

--- rn-assignment7-11115682/App.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
-
-export default function App() {
-  return <AppNavigator />;
-}
